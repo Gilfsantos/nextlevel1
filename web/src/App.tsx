3d53857fb1a2e2f18a1ad0fb42e558f2980da5d2
Gilmar <66905710+Gilfsantos@users.noreply.github.com>
2020-12-13 18:10:26 -0300

    start server
    30 start server

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -5,6 +5,7 @@ import './App.css';
 import Header from './Header';
 
 function App() {
+  const counter = 1
   return (
     <div>
       <Header title = "Hello World" />
@@ -12,6 +13,9 @@ function App() {
       <Header title = "sabao do mar" />
       <Header title = "pagina virada" />
       
+
+      <h1>{counter}</h1>
+      <button type="button" onClick={console.log}>aumentar</button>
 
       <h1>Conteudo da aplicaçao</h1>
     </div>
