7fdd11841b6bacdc7c4e0e65eb9f42408241ab1f
Gilmar <66905710+Gilfsantos@users.noreply.github.com>
2021-04-02 11:27:26 -0300

    continua os estudos
    49 e o numero

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,25 +1,15 @@
 import React from 'react';
 
 import './App.css';
+import Home from './pages/Home';
 
-import Header from './Header';
 
 function App() {
-  const counter = 1
   return (
-    <div>
-      <Header title = "Hello World" />
-      <Header title = "cachorro doido" />
-      <Header title = "sabao do mar" />
-      <Header title = "pagina virada" />
-      
-
-      <h1>{counter}</h1>
-      <button type="button" onClick={console.log}>aumentar</button>
-
-      <h1>Conteudo da aplicaçao</h1>
-    </div>
-  );
-}
+    
+  <Home />
+  
+);
+  }
 
 export default App;
